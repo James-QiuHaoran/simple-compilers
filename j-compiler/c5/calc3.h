5e5727d91e0dd1498ffba96ba4f3a42b23c349b4
James-QiuHaoran <jamesqiu@hku.hk>
2019-04-07 00:39:57 +0800

    finished dev

--- j-compiler/c5/calc3.h
+++ j-compiler/c5/calc3.h
@@ -1,23 +1,58 @@
-typedef enum { typeCon, typeId, typeOpr } nodeEnum;
-typedef enum { varTypeInt, varTypeChar, varTypeString } varTypeEnum;
+#include "strmap.h"
+
+#define GLOBAL_SIZE 100
+#define LOCAL_SIZE 100
+#define FUNC_SIZE 100
+
+#define LABEL_NAME_L 6
+#define REG_NAME_L 100
+
+typedef enum { typeCon, typeId, typeOpr, typeFunc } nodeEnum;
+typedef enum { varTypeInt, varTypeChar, varTypeStr, varTypeNil } varTypeEnum;
+typedef enum { typeFuncList, typeStmtList } listTypeEnum;
 
 /* constants */
-typedef struct {
-    long value;                 /* value of constant */
+typedef struct conNodeType {
+    int value;                 /* value of constant */
+    char strValue[1023];        /* string value, max 1023 chars*/
     varTypeEnum type;
 } conNodeType;
 
 /* identifiers */
-typedef struct {
-    int i;                      /* subscript to sym array */
+typedef struct idNodeType {
+    char varName[12];           /* subscript to sym array */
+    varTypeEnum type;
 } idNodeType;
 
 /* operators */
-typedef struct {
+typedef struct oprNodeType {
     int oper;                   /* operator */
     int nops;                   /* number of operands */
     struct nodeTypeTag *op[1];  /* operands (expandable) */
 } oprNodeType;
+
+/* functions */
+typedef struct funcNodeType {
+    char name[12];
+    struct nodeTypeTag *args;
+    struct nodeTypeTag *stmt;
+    int num_args;
+    int num_local_vars;
+} funcNodeType;
+
+/* nodes, for funcs and stmts */
+typedef struct nodeInListType {
+    struct nodeTypeTag *node;
+    struct nodeInListType *next;
+} nodeInListType;
+
+/* linked lists, for funcs and stmts */
+typedef struct nodeLinkedListType {
+    listTypeEnum type;          /* type of node list */
+    int num_nodes;              /* number of nodes */
+    struct nodeInListType *head;
+    struct nodeInListType *tail;
+} nodeLinkedListType;
 
 typedef struct nodeTypeTag {
     nodeEnum type;              /* type of node */
@@ -28,7 +63,22 @@ typedef struct nodeTypeTag {
         conNodeType con;        /* constants */
         idNodeType id;          /* identifiers */
         oprNodeType opr;        /* operators */
+        funcNodeType func;      /* functions*/
     };
 } nodeType;
 
-extern int sym[26];
+typedef struct StackSym {
+    StrMap *symbol_table;
+    int num_args;
+    int num_local_vars;
+    struct StackSym *lower;
+} StackSym;
+
+/* symbol tables */
+extern StrMap* globalSym;
+extern StrMap* funcSym;
+extern StackSym* localSym;
+
+/* stack list (functions and statements) */
+extern nodeLinkedListType* funcs;
+extern nodeLinkedListType* stmts;
